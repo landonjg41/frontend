4c1c5a3aa5bf7e2f5ae48c8fe27ed7cee0d843db
Landon Graham <landonjg41@gmail.com>
2024-03-06 09:06:45 -0700

    ncaa commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import collegeData from './CollegeBasketballTeams.json';
+
+interface CollegeProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Heading() {
+  return (
+    <div>
+      <h1>Welcome to the NCAA March Madness Hub</h1>
+      <p>
+        This website is your ultimate guide to the NCAA Basketball March Madness
+        Tournament. Here, you'll find detailed information on all participating
+        colleges, including team names, mascots, and locations. Dive into the
+        excitement of March Madness and get to know the teams competing for the
+        championship!
+      </p>
+    </div>
+  );
+}
+
+class TeamCard extends React.Component<CollegeProps> {
+  render() {
+    return (
+      <div className="team-card">
+        <h2>{this.props.school}</h2>
+        <h3>Masoct: {this.props.name}</h3>
+        <h4>Location: {this.props.city}</h4>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+function SchoolInfo() {
+  return (
+    <div className="teams-container">
+      {collegeData.teams.map((oneSchool) => (
+        <TeamCard {...oneSchool} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <br></br>
+      <SchoolInfo />
     </div>
   );
 }
